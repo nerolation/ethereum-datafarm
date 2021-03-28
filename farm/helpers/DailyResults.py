9ec2e3bc5478dca4454ab92d5ad7876d7acb80b8
Anton Wahrstätter <51536394+Nerolation@users.noreply.github.com>
2021-03-28 15:00:19 +0200

    Add files via upload

--- farm/helpers/DailyResults.py
+++ farm/helpers/DailyResults.py
@@ -1,21 +1,9 @@
-import sha3
-import numpy as np
 from datetime import datetime
 import pandas as pd
-import re
-import requests
-import time
-import json
 import io
 import csv
 import boto3
-from farm.helpers.prepare_event_helper import from_hex, prepare_event
 
-API = 'https://api.etherscan.io/api?module=logs&action=getLogs'  # API endpoint
-APIQuery = '{}&fromBlock={}&toBlock={}&topic0={}&topic0_1_opr=and&address={}&apikey={}'
-# Load API KEY
-with open(".apikey/key.txt") as k:
-    KEY = str(k.read().strip())
 
 # AWS Stuff
 with open(".aws/credentials") as creds:
@@ -26,16 +14,6 @@ bucket = s3_res.Bucket("ethereum-datahub")
 s3 = boto3.client('s3', aws_access_key_id=creds[0][0], aws_secret_access_key=creds[1][0])
 # END AWS Stuff
 
-
-
-def from_unix(time):
-    try:
-        return time.strftime("%d/%m/%Y  %H:%M:%S")
-    except:
-        return datetime.utcfromtimestamp(time).strftime("%d/%m/%Y %H:%M:%S")
-                
-def get_method_from_canonical_expression(method):
-    return '0x' + sha3.keccak_256(method.encode('utf-8')).hexdigest()
 
 
 class DailyResults():
@@ -115,107 +93,4 @@ class DailyResults():
             handle.write(str(chunk.iloc[-1]['blocknumber']))
 
         return True
-
-class Method:
-    def __init__(self, method):
-        self.canonicalExpression = method
-        self.id = get_method_from_canonical_expression(method)
-
-class Contract:
-    def __init__(self, addr, name, method, startblock, chunksize = 2000):
-        self.addr = addr
-        self.name = name
-        self.method = Method(method)
-        self.startBlock = startblock
-        self.chunksize = int(chunksize)
-        self.chunksizeLock = False
-        self.fromBlock = int(self.startBlock)
-        self.chunksizeAdjuster = np.array([400*1.3]*10)
-        self.fileCounter = 0
-        self.DailyResults = DailyResults(self.name, datetime.now())
-        self.lastScrapedBlock = 0
-        self.path = None
-    
-    # Check if the average number of results of the the last 10 request has to 
-    # less elements and adjust the chunksize if applicable
-    def chunksize_could_be_larger(self):
-        if np.mean(self.chunksizeAdjuster) < 400 and self.chunksize < 5000:
-            return True
-        return False
-    
-    
-    # Increase Chunksize
-    def increase_chunksize(self):
-        self.chunksize = round(self.chunksize*2) if self.chunksize < 5000 else self.chunksize
-        #print('... increasing chunksize for {} to {}'.format(self.name,self.chunksize))
-        return
-    
-    def mine(self, query, methodId):
-        chunk = []
-        for e in query:
-            toChunk = prepare_event(e, methodId)
-            chunk.append(toChunk)
-        return chunk
-    
-    
-    def query_API(self):
-        res = None
-        # Create the actual API Request
-        queryString = APIQuery.format(API, 
-                                       self.fromBlock, 
-                                       int(self.fromBlock)+int(self.chunksize), 
-                                       self.method.id, 
-                                       self.addr, 
-                                       KEY)
-        
-       
-        # Submit Request
-        res = json.loads(requests.get(queryString).content) 
-        
-        # Catch fails
-        
-        if res['message'] == 'No records found':
-            self.fromBlock += self.chunksize + 1
-            self.chunksizeAdjuster = np.append(self.chunksizeAdjuster,[0])[-10:]
-            return None
-        
-        if (res['status'] == '0' or not res):
-            print('... request failed for {}'.format(self.addr))
-            time.sleep(10)
-            return
-        
-        # Check if len of returned results is the maximum of 1000
-        # If so, enter recursive mode with a smaller chunksize - try again
-        if (len(res['result']) >= 1000): #Request to large
-            self.chunksize -= round(self.chunksize / 3)
-            #print('... decreasing chunksize for {} to {}'.format(self.name,self.chunksize))
-            return self.query_API() # Recursive bby
-        
-        self.chunksizeAdjuster = np.append(self.chunksizeAdjuster,[len(res['result'])])[-10:]
-        self.fromBlock += self.chunksize + 1
-        
-        return res['result']
-    
-    
-    def log_to_console(self, res):
-        ts = from_unix(datetime.now())
-        log = "{:^23}-{:^18}|{:^10}-{:^10}| {:^18} |{:^6}|{:^6}|{:^6}".format(
-                                                                              ts, 
-                                                                              self.name,
-                                                                              res[0][1],
-                                                                              res[-1][1], 
-                                                                              from_unix(res[-1][0]), 
-                                                                              len(res), 
-                                                                              self.chunksize, 
-                                                                              self.fileCounter
-                                                                             )
-        return log
-
-    
-    def __repr__(self):
-        method = re.search("(.*)\(.*", self.method.canonicalExpression).group(1)
-        return "Contract @ {} » Method: '{}' » Startblock: {:,}".format(self.addr, method, self.fromBlock)
-
-
-
 
--- farm/helpers/load_contract_helper.py
+++ farm/helpers/load_contract_helper.py
@@ -1,6 +1,47 @@
 import re
+import os.path
+import datetime
+import pandas as pd
 from farm.helpers.Contract import Contract
+from farm.helpers.DailyResults import s3_res, s3
+from botocore.exceptions import ClientError
 
+
+def restore_fromBlock_from_AWS(contract):
+    for date in (datetime.datetime.now() - datetime.timedelta(days=i+365) for i in range(1000)):
+        fileKey = "{}_{}/csv/{}.csv".format(contract.name,
+                                            contract.method.canonicalExpression.split("(")[0].lower(),
+                                            date.strftime("%d_%m_%Y"))
+        try:
+            obj = s3.get_object(Bucket = "ethereum-datahub", Key = fileKey)
+            objB = obj['Body']
+            df = pd.read_csv(objB)   
+            contract.fromBlock = df.iloc[-1]['blocknumber']+1
+            print("'fromBlock' successfully loaded from AWS...")
+            if not os.path.exists('{}/lastSafedBlock/'.format(contract.path)):
+                os.mkdir('{}/lastSafedBlock/'.format(contract.path))
+            with open('{}/lastSafedBlock/{}_{}.txt'.format(contract.path, 
+                                                           contract.name, 
+                                                           contract.method.canonicalExpression.split("(")[0].lower()), 'w') as handle:
+                handle.write(str(contract.fromBlock))
+            return
+            
+        except ClientError as ex:
+            if ex.response['Error']['Code'] == 'NoSuchKey':
+                continue
+            else:
+                print(ex)
+                break
+        except:
+            print("FAIL")
+            break
+
+        
+
+
+
+    
+    
 def load_contracts(contracts=[], start=True, location="contracts"):
     if start:
         print("Loading new contracts ...\n")
@@ -33,10 +74,13 @@ def load_contracts(contracts=[], start=True, location="contracts"):
 
                 try:
                     filename =location+"/lastSafedBlock/"+contracts[-1].name+"_"+contracts[-1].method.canonicalExpression.split("(")[0].lower()
-                    with open('{}.txt'.format(filename), 'r') as loadBlock:
-                        contracts[-1].fromBlock = int(loadBlock.read())+1
-                        print("`Startblock` overwritten for {} to Block {:,}\n".format(contracts[-1].name,
-                                                                                       contracts[-1].fromBlock))
+                    if os.path.exists('{}.txt'.format(filename)):
+                        with open('{}.txt'.format(filename), 'r') as loadBlock:
+                            contracts[-1].fromBlock = int(loadBlock.read())+1
+                            print("`Startblock` overwritten for {} to Block {:,}\n".format(contracts[-1].name,
+                                                                                           contracts[-1].fromBlock))
+                    else:
+                        restore_fromBlock_from_AWS(contracts[-1])
                 except:
                     pass
     return contracts
