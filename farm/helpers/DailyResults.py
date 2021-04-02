9c140295fef734c706afdc596567c6beb437dc4d
Anton Wahrstätter <51536394+Nerolation@users.noreply.github.com>
2021-04-03 00:34:20 +0200

    Add files via upload

---
+++ farm/helpers/DailyResults.py
@@ -0,0 +1,99 @@
+from datetime import datetime
+import pandas as pd
+import io
+import csv
+import boto3
+
+
+# AWS Stuff
+with open(".aws/credentials.txt") as creds:
+    reader = csv.reader(creds)
+    creds = [i for i in reader]
+s3_res = boto3.resource('s3', aws_access_key_id=creds[0][0], aws_secret_access_key=creds[1][0])
+s3 = boto3.client('s3', aws_access_key_id=creds[0][0], aws_secret_access_key=creds[1][0])
+# END AWS Stuff
+
+
+
+class DailyResults():
+    def __init__(self, name, init, deltaToEnd = 100):
+        self.name = name
+        self.init = init
+        self.results = pd.DataFrame()
+    
+    # Add column with day of the month to the dataFrame
+    # This is needed to split the dataFrame between daily chunks to save them separately
+    def enrich_daily_results_with_day_of_month(self, chunk):
+        chunk = pd.DataFrame(chunk)
+        chunk["day"] = chunk.apply(lambda x: datetime.utcfromtimestamp(x[0]).strftime("%d"), axis=1)
+        return chunk
+        
+    # Try to save the file / sync to AWS if there are two different days of month in the results
+    # ...this means that got a transition from one day to another
+    def try_to_save_day(self, results, contract, aws_bucket):
+        # This helps for entering recursive mode
+        # When the dataFrame is split, it can happen that nothing remains but the empty dataFrame
+        if results.empty == True:
+            print("Empty Dataframe...")
+            return False
+        
+        # Get day of month (ex. 04) for the first entry in the results
+        if self.results.empty:
+            firstEntry = results.iloc[0]["day"]
+        else:
+            firstEntry = self.results.iloc[0]["day"]
+  
+        # Get day of month (ex. 04) for the last entry in the results
+        lastEntry = results.iloc[-1]["day"]
+        
+        # No save cause day isn't over...probably
+        if firstEntry == lastEntry:
+            self.results = self.results.append(results)
+            return True
+        
+        # Different days in the results
+        else:
+            # First day in the dataFrame will be saved
+            res = results[results['day'] == firstEntry]
+            self.results = self.results.append(res)
+            self.save_results(self.results, contract, aws_bucket)
+            contract.fileCounter += 1
+            self.results = pd.DataFrame()
+            # Second part including other days than the one save will be split and 
+            # are thrown into the function again
+            rest = results[results['day'] != firstEntry]
+            return self.try_to_save_day(rest, contract, aws_bucket)
+            
+    def save_results(self, chunk, contract, aws_bucket):
+        del chunk['day']
+        filename = datetime.utcfromtimestamp(chunk.iloc[0][0]).strftime("%d_%m_%Y")
+        
+        chunk.columns = contract.headerColumn
+        
+        csv_buf = io.StringIO()
+        pickle_buf = io.BytesIO()
+        chunk.to_csv(csv_buf, index = False)
+        chunk.to_pickle(pickle_buf)
+        s3.put_object(Body = csv_buf.getvalue(), 
+                      Bucket = aws_bucket, 
+                      Key = 'contracts/{}_{}/csv/{}.csv'.format(contract.name, 
+                                                                contract.method.canonicalExpression.split("(")[0].lower(),
+                                                                filename))
+        s3.put_object(Body = pickle_buf.getvalue(), 
+                      Bucket = aws_bucket, 
+                      Key = 'contracts/{}_{}/pickle/{}.pickle'.format(contract.name, 
+                                                                      contract.method.canonicalExpression.split("(")[0].lower(),
+                                                                      filename))
+        # print("Saved Chunk to AWS S3 as `{}_{}/{}.(csv|pickle)`".format(contract.name, 
+                                                                        # contract.method.canonicalExpression.split("(")[0].lower(),
+                                                                      #  filename))
+                
+                
+        fK = 'config/{}/lastSafedBlock/{}_{}.txt'.format(contract.path,
+                                                         contract.name,
+                                                         contract.method.canonicalExpression.split("(")[0].lower())
+        s3.put_object(Body=str(chunk.iloc[-1]['blocknumber']),Bucket=aws_bucket,Key=fK)
+      
+
+        return True
+
---
+++ farm/helpers/EventHelper.py
@@ -0,0 +1,133 @@
+def from_hex(string):
+    if str(string) == '0x':
+        return 0
+    return int(str(string),16)
+
+def get_header_columns(methodId):
+    if methodId in ["0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef"]:# Transfer(addr,addr,uint256)
+        return ['timestamp','blocknumber','txhash','txindex','logindex',
+                'txfrom','txto', 'txvalue', 'gas_price', 'gas_used']
+    
+    elif methodId in ["0x8c5be1e5ebec7d5bd14f71427d1e84f3dd0314c0f7b2291e5b200ac8c7c3b925"]: # Approval(addr,addr,uint256)
+        return ['timestamp','blocknumber','txhash','txindex','logindex',
+                'owner','spender', 'txvalue', 'gas_price', 'gas_used']
+                    
+    elif methodId in ["0xf5c174d57843e57fea3c649fdde37f015ef08750759cbee88060390566a98797", # SupplyIncreased(address,uint256)
+                      "0x0f6798a560793a54c3bcfe86a93cde1e73087d944c0ea20544137d4121396885", # Mint(address,uint256)
+                      "0xc65a3f767206d2fdcede0b094a4840e01c0dd0be1888b5ba800346eaa0123c16"  # Issue(address,uint256)
+                     ]: 
+        return ['timestamp','blocknumber','txhash','txindex','logindex',
+                'to', 'value', 'gas_price', 'gas_used']    
+    
+    elif methodId in ["0x1b7e18241beced0d7f41fbab1ea8ed468732edbcb74ec4420151654ca71c8a63", # SupplyDecreased(address,uint256)
+                      "0x222838db2794d11532d940e8dec38ae307ed0b63cd97c233322e221f998767a6"  # Redeem(address,uint256)
+                     ]: 
+        return ['timestamp','blocknumber','txhash','txindex','logindex',
+                'from', 'value', 'gas_price', 'gas_used'] 
+    
+    elif methodId in ["0xcc16f5dbb4873280815c1ee09dbd06736cffcc184412cf7a71a0fdb75d397ca5"]: # Burn(address,uint256)
+        return ['timestamp','blocknumber','txhash','txindex','logindex',
+                'burner', 'value', 'gas_price', 'gas_used']
+    
+    elif methodId in ["0xcb8241adb0c3fdb35b70c24ce35c5eb0c17af7431c99f827d44a445ca624176a"]: # Issue(uint256)
+        return ['timestamp','blocknumber','txhash','txindex','logindex','value', 'gas_price', 'gas_used'] 
+    
+    elif methodId in ["0x702d5967f45f6513a38ffc42d6ba9bf230bd40e8f53b16363c7eb4fd2deb9a44"]: # Redeem(uint256)
+        return ['timestamp','blocknumber','txhash','txindex','logindex',
+                'value', 'gas_price', 'gas_used']
+    
+    elif methodId in ["0x61e6e66b0d6339b2980aecc6ccc0039736791f0ccde9ed512e789a7fbdd698c6"]: # DestroyedBlackFunds(addr,uint256)
+        return ['timestamp','blocknumber','txhash','txindex','logindex',
+                'blackListedUser', 'value', 'gas_price', 'gas_used']
+    
+    elif methodId in ["0xab8530f87dc9b59234c4623bf917212bb2536d647574c8e7e5da92c2ede0c9f8"]: # Mint(addr,addr,uint256)
+        return ['timestamp','blocknumber','txhash','txindex','logindex',
+                'minter', "to" , 'value', 'gas_price', 'gas_used']
+    
+    
+
+
+def prepare_event(e, methodId):
+    if methodId in ["0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef", # Transfer
+                    "0x8c5be1e5ebec7d5bd14f71427d1e84f3dd0314c0f7b2291e5b200ac8c7c3b925",  # Approval
+                    "0xab8530f87dc9b59234c4623bf917212bb2536d647574c8e7e5da92c2ede0c9f8"
+                   ]: 
+        va = from_hex(e['data'])
+        bn = from_hex(e['blockNumber'])
+        ts = from_hex(e['timeStamp'])
+        th = e['transactionHash']
+        ti = from_hex(e['transactionIndex'])
+        gp = from_hex(e['gasPrice'])
+        gu = from_hex(e['gasUsed'])
+        li = from_hex(e['logIndex'])
+        tf = '0x' + e['topics'][1][-40:]
+        tt = '0x' + e['topics'][2][-40:]
+        return [ts,bn,th,ti,li,tf,tt,va,gp,gu]
+    
+    
+    elif methodId in ["0xcb8241adb0c3fdb35b70c24ce35c5eb0c17af7431c99f827d44a445ca624176a", # Issue
+                      "0x702d5967f45f6513a38ffc42d6ba9bf230bd40e8f53b16363c7eb4fd2deb9a44", # Redeem
+                     ]:            
+        va = from_hex(e['data'])
+        bn = from_hex(e['blockNumber'])
+        ts = from_hex(e['timeStamp'])
+        th = e['transactionHash']
+        ti = from_hex(e['transactionIndex'])
+        gp = from_hex(e['gasPrice'])
+        gu = from_hex(e['gasUsed'])
+        li = from_hex(e['logIndex'])
+        return [ts,bn,th,ti,li,va,gp,gu]
+    
+    elif methodId in ["0xf5c174d57843e57fea3c649fdde37f015ef08750759cbee88060390566a98797", # SupplyIncreased
+                      "0x1b7e18241beced0d7f41fbab1ea8ed468732edbcb74ec4420151654ca71c8a63", # SupplyDecreased
+                      "0x0f6798a560793a54c3bcfe86a93cde1e73087d944c0ea20544137d4121396885", # Mint
+                      "0xcc16f5dbb4873280815c1ee09dbd06736cffcc184412cf7a71a0fdb75d397ca5", # Burn
+                      "0xc65a3f767206d2fdcede0b094a4840e01c0dd0be1888b5ba800346eaa0123c16", # Issue
+                      "0x222838db2794d11532d940e8dec38ae307ed0b63cd97c233322e221f998767a6"  # Redeem
+                     ]:
+                      
+        va = from_hex(e['data'])
+        bn = from_hex(e['blockNumber'])
+        ts = from_hex(e['timeStamp'])
+        th = e['transactionHash']
+        ti = from_hex(e['transactionIndex'])
+        gp = from_hex(e['gasPrice'])
+        gu = from_hex(e['gasUsed'])
+        li = from_hex(e['logIndex'])
+        tf = '0x' + e['topics'][1][-40:]
+        return [ts,bn,th,ti,li,tf,va,gp,gu]
+    
+    
+    elif methodId in ["0x61e6e66b0d6339b2980aecc6ccc0039736791f0ccde9ed512e789a7fbdd698c6"]: # DestroyedBlackFunds
+        bn = from_hex(e['blockNumber'])
+        ts = from_hex(e['timeStamp'])
+        th = e['transactionHash']
+        ti = from_hex(e['transactionIndex'])
+        gp = from_hex(e['gasPrice'])
+        gu = from_hex(e['gasUsed'])
+        li = from_hex(e['logIndex'])
+        da1 = '0x' + e['data'][26:66]
+        da2 = from_hex('0x' + e['data'][-20:])
+        return [ts,bn,th,ti,li,da1,da2,gp,gu]
+    
+    
+    else:
+        bn = from_hex(e['blockNumber'])
+        ts = from_hex(e['timeStamp'])
+        th = e['transactionHash']
+        ti = from_hex(e['transactionIndex'])
+        gp = from_hex(e['gasPrice'])
+        gu = from_hex(e['gasUsed'])
+        li = from_hex(e['logIndex'])
+        da = e['data']
+        chunk = [ts,bn,th,ti,li,gp,gu]
+        if len(e['topics'])>1:
+            to1 = e['topics'][1]
+            chunk.append(to1)
+        if len(e['topics'])>2:
+            to2 = e['topics'][2]
+            chunk.append(to2)
+        if len(e['topics'])>3:
+            to3 = e['topics'][3]
+            chunk.append(to3)
+        return chunk
