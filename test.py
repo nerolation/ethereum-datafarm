85755f9a3497c3005e5c12012739abddf4881297
Anton Wahrstätter <51536394+Nerolation@users.noreply.github.com>
2021-04-07 20:18:09 +0200

    Strip test.py

--- test.py
+++ test.py
@@ -1,23 +1,13 @@
 from farm.Farm import *
 
-# Your AWS bucket
-aws_bucket = "ethereum-datahub"
-
-# Path to Etherscan API key
-keyPath = ".apikey/key2.txt"
-
+aws_bucket = "ethereum-datahub" # Your AWS bucket
 
 # Run
 if __name__=="__main__":
     
     # Load contracts
-    # @param config_location Location of config file (contracts.csv)
-    # @param aws_bucket Aws bucket name
-    contracts = load_contracts(config_location="contracts2", aws_bucket=aws_bucket)
+    contracts = load_contracts(aws_bucket=aws_bucket)
 
     # Initialize Farm and get status
-    #
-    # @param contracts Loaded contracts array
-    # @param keyPath Path to API key
-    farm = Farm(contracts=contracts, keyPath=keyPath, aws_bucket=aws_bucket).status()
+    farm = Farm(contracts=contracts,aws_bucket=aws_bucket).status()
     farm.start_farming()
