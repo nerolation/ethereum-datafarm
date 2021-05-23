57dea57f0a5a5cb07f75b3b5a95065e6f5b32235
Anton Wahrstätter <51536394+Nerolation@users.noreply.github.com>
2021-05-23 14:17:49 +0200

    Add files via upload

--- farm/helpers/Logger.py
+++ farm/helpers/Logger.py
@@ -1,40 +1,40 @@
 import logging, sys, time
-try:
-    logging.basicConfig(format='%(message)s',filename='logs/datafarm.log', level=logging.INFO)
-except FileNotFoundError:
-    logging.basicConfig(format='%(message)s',filename='../../logs/datafarm.log', level=logging.INFO)
-
 
 class Logger:
-        def animation(self, string=None):
-
-            if string:
-                sys.stdout.write(string)
-                sys.stdout.flush()
-            sys.stdout.write(".")
+    def __init__(self):
+        self.activatedLogger = False
+        
+    def animation(self, string=None):
+        if string:
+            sys.stdout.write(string)
             sys.stdout.flush()
-            time.sleep(0.8)
-            sys.stdout.write(".")
-            sys.stdout.flush()
-            time.sleep(0.8)
-            sys.stdout.write(".")
-            sys.stdout.flush()
-            time.sleep(1)
-            print("\n")
+        sys.stdout.write(".")
+        sys.stdout.flush()
+        time.sleep(0.8)
+        sys.stdout.write(".")
+        sys.stdout.flush()
+        time.sleep(0.8)
+        sys.stdout.write(".")
+        sys.stdout.flush()
+        time.sleep(1)
+        print("\n")
 
-        def activateLogger(self):
-            global activatedLogger
-            activatedLogger = True
-            return self
+    def activateLogger(self):
+        self.activatedLogger = True
+        return self
 
 
-        def logprint(self, content, animated=False):
-            if animated:
-                self.animation(content)
-            else:
-                print(content)
-            if activatedLogger:
-                logging.info(content)
+    def logprint(self, content, animated=False):
+        if animated:
+            self.animation(content)
+        else:
+            print(content)
+        if self.activatedLogger:
+            logging.info(content)
 
-                
-globalLogger=Logger().activateLogger().logprint
+try:
+    logging.basicConfig(format='%(message)s',filename='logs/datafarm.log', level=logging.INFO)
+    globalLogger=Logger().activateLogger().logprint
+except FileNotFoundError:
+    print("No `logs` folder found. No logs will be stored...")
+    globalLogger=Logger().logprint       
