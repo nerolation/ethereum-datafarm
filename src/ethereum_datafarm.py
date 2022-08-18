2b81d88fbacc0afedf6c7f9aabeb208636b4f0ea
Anton Wahrstätter <51536394+Nerolation@users.noreply.github.com>
2022-08-18 14:10:54 +0200

    Update ethereum_datafarm.py

--- src/ethereum_datafarm.py
+++ src/ethereum_datafarm.py
@@ -239,8 +239,8 @@ class Contract():
     
     def log_chunk_size(self, old_size, op):
         avg = sum(self.avgNrOfPages)/len(self.avgNrOfPages)
-        msg = "{} chunk size        | {:<21}".format(op, self.printName[:17]+"...") \
-            + " {:>5,.0f} --> {:<5,.0f} with avg. pages of {:.2f}".format(old_size, self.chunksize, avg) 
+        msg = "{} chunk size for {}".format(op, self.printName[:17]+"...:") \
+            + " {:,.0f} --> {:,.0f} with avg. pages of {:.2f}".format(old_size, self.chunksize, avg) 
         print(INFO_MSG.format(msg))
         
     def log_nothing_found(self):
